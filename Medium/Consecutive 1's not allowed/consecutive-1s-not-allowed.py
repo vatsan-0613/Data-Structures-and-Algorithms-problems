61f4b88e78dfbf5d54a58e476db7795b914976e9
SRIVATSAN S <95332677+vatsan-0613@users.noreply.github.com>
2023-12-13 23:56:09 +0530

    Updated Solution - GfG to GitHub

--- Medium/Consecutive 1's not allowed/consecutive-1s-not-allowed.py
+++ Medium/Consecutive 1's not allowed/consecutive-1s-not-allowed.py
@@ -3,19 +3,13 @@ class Solution:
 
 	def countStrings(self,n):
     	# code here
-    	if n == 1:
-            return 2
-        elif n == 2:
-            return 3
-    
-        dp = [0] * (n + 1)
-        dp[1] = 2
-        dp[2] = 3
-    
-        for i in range(3, n + 1):
-            dp[i] = (dp[i - 1] + dp[i - 2])%(10**9+7)
-    
-        return dp[n]%(10**9+7)
+    	a = 1
+    	b = 1 
+    	mod = (10**9 + 7)
+    	for i in range(n-1):
+    	    a, b = (a+b) %mod, a 
+    	
+    	return (a+b)%mod
 
 
 #{ 
